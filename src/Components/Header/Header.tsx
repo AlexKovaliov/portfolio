94775e0640b5f9c684cc44286cee078968d87323
AlexKovaliov <lawsansana@gmail.com>
2020-09-19 21:39:09 +0300

    started to creating my own portfolio

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function Header () {
+    return (
+        <div>
+            <div>
+                <span>Main</span>
+                <span>Skills</span>
+                <span>Work</span>
+                <span>Contacts</span>
+            </div>
+        </div>
+    )
+}
---
+++ src/Components/WelcomeBlock/WelcomeBlock.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function WelcomeBlock() {
+    return (
+        <div>
+            <div>
+                <span>Hello!
+                    <br/>  My name is Alexander Kovaliov.
+                    <br/> I'm a front-end developer
+                </span>
+                <span> --Here will be img--</span>
+            </div>
+        </div>
+    )
+}
