ef26faa98ac662a6724d351ceacdce625a75ce9a
AlexKovaliov <lawsansana@gmail.com>
2020-12-06 21:45:05 +0300

    refactoring navigation in Nav used react-scroll; smooth scrolling; active style

--- src/Components/Header/Nav/Nav.tsx
+++ src/Components/Header/Nav/Nav.tsx
@@ -1,15 +1,48 @@
 import React from 'react';
 import s from './Nav.module.scss'
+import {Link} from 'react-scroll';
 
 
 export function Nav() {
 
     return (
         <div className={s.nav}>
-            <a href="#main">Main</a>
-            <a href="#skills">Skills</a>
+            {/*<a href="#main">Main</a>*/}
+            {/* <a href="#skills">Skills</a>
             <a href="#works">Works</a>
-            <a href="#contacts">Contacts</a>
+            <a href="#contacts">Contacts</a>*/}
+            <Link
+                activeClass={s.active}
+                to="main"
+                spy={true}
+                smooth={true}
+                offset={1}
+                duration={500}
+            >Main</Link>
+            <Link
+                activeClass={s.active}
+                to="skills"
+                spy={true}
+                smooth={true}
+                offset={1}
+                duration={500}
+            >Skills</Link>
+            <Link
+                activeClass={s.active}
+                to="works"
+                spy={true}
+                smooth={true}
+                offset={1}
+                duration={500}
+            >Works</Link>
+            <Link
+                activeClass={s.active}
+                to="contacts"
+                spy={true}
+                smooth={true}
+                offset={-90}
+                duration={500}
+            >Contacts</Link>
         </div>
     )
 }
