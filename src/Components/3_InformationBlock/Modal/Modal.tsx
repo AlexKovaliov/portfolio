6aac783f2f1c45902bb37838d48f9039c23ccb36
AlexKovaliov <lawsansana@gmail.com>
2020-12-25 14:55:21 +0300

    close btn for modal

--- src/Components/3_InformationBlock/Modal/Modal.tsx
+++ src/Components/3_InformationBlock/Modal/Modal.tsx
@@ -4,6 +4,7 @@ import {Button} from "../../../common/generalComponents/Button/Button";
 //images
 import img_eng_cv from "../../../assets/img/img_eng_cv.jpg"
 import img_rus_cv from "../../../assets/img/img_rus_cv.jpg"
+import closeBtn from "../../../assets/iconsFonts/close.svg"
 
 
 export function Modal() {
@@ -15,8 +16,16 @@ export function Modal() {
     const imgRusCv = {
         backgroundImage: `url(${img_rus_cv})`
     }
+
+    const cBtn = {
+        backgroundImage: `url(${closeBtn})`
+    }
+
     return (
         <div className={s.container}>
+
+            <div className={s.closeBtn} style={cBtn}></div>
+
             <div className={s.modal}>
                 <div className={s.cover}>
                     <div className={s.imgCv} style={imgRusCv}>
